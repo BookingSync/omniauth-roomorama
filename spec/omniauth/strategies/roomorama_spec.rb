89ba179f2d38f90afebd51f17c48eb54dc1775e7
Sébastien Grosjean <public@zencocoon.com>
2013-10-22 21:03:48 +0800

    Fix URLs and tests

--- spec/omniauth/strategies/roomorama_spec.rb
+++ spec/omniauth/strategies/roomorama_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 require 'omniauth-roomorama'
 
 describe OmniAuth::Strategies::Roomorama do
+  before do
+    OmniAuth.config.test_mode = true
+  end
+
   subject do
     OmniAuth::Strategies::Roomorama.new(nil, @options || {})
   end
@@ -10,15 +14,15 @@ describe OmniAuth::Strategies::Roomorama do
 
   describe '#client' do
     it 'should have the correct site' do
-      subject.client.site.should eq("https://roomorama.com")
+      subject.client.site.should eq("https://api.roomorama.com")
     end
 
     it 'should have the correct authorization url' do
-      subject.client.options[:authorize_url].should eq("/oauth/authorize")
+      subject.client.options[:authorize_url].should eq("https://roomorama.com/oauth/authorize")
     end
 
     it 'should have the correct token url' do
-      subject.client.options[:token_url].should eq('/oauth/token')
+      subject.client.options[:token_url].should eq('https://roomorama.com/oauth/token')
     end
   end
 
